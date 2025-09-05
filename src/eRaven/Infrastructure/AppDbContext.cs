5112ee5f50b44d638feb50835f0a2b849868c944
Khrapal Maksym <khrapal_m_g@outlook.com>
2025-09-05 23:57:07 +0300

    test(domain): додано PersonPositionAssignmentTests (окреме покриття сутності)
    - Перевірка CLR-дефолтів
    - Встановлення/зчитування базових властивостей
    - Семантика відкритого/закритого інтервалу
    - Узгодженість навігаційних посилань Person/PositionUnit
    - Валідація без DataAnnotations (очікувано порожня)
    - Присвоєння ModifiedUtc

--- src/eRaven/Infrastructure/AppDbContext.cs
+++ src/eRaven/Infrastructure/AppDbContext.cs
@@ -14,6 +14,7 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
 {
     public DbSet<Order> Orders => Set<Order>();
     public DbSet<Person> Persons => Set<Person>();
+    public DbSet<PersonPositionAssignment> PersonPositionAssignments => Set<PersonPositionAssignment>();
     public DbSet<PersonStatus> PersonStatuses => Set<PersonStatus>();    
     public DbSet<Plan> Plans => Set<Plan>();
     public DbSet<PlanParticipantSnapshot> PlanParticipantSnapshots => Set<PlanParticipantSnapshot>();
