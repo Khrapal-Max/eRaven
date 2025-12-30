a9fbe45401b5e97c8804fac31ba37496e02393a0
Khrapal Maksym <khrapal_m_g@outlook.com>
2025-12-30 15:58:35 +0200

    Add entity Rank and entity configuration

--- src/eRaven/Infrastructure/Configurations/PositionUnitConfiguration.cs
+++ src/eRaven/Infrastructure/Configurations/PositionUnitConfiguration.cs
@@ -64,7 +64,8 @@ public class PositionUnitConfiguration : IEntityTypeConfiguration<PositionUnit>
 
         e.Property(x => x.IsActived)
          .HasColumnName("is_active")
-         .HasDefaultValue(true);
+         .HasDefaultValue(true)
+         .IsRequired();
 
         // ===============================
         // Indexes
---
+++ src/eRaven/Migrations/20251230135458_AddRank.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eRaven.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRank : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ranks",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    title = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: false),
+                    priority = table.Column<int>(type: "int", nullable: false),
+                    is_active = table.Column<bool>(type: "boolean", nullable: false, defaultValue: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ranks", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ranks_title",
+                table: "ranks",
+                column: "title",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ranks");
+        }
+    }
+}
